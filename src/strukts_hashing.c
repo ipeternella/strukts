10474b2d163090592c1ad26793fd1beceb94917a
IgooorGP <igor.feq@gmail.com>
2021-08-12 19:13:14 -0300

    feat(murmur3): improve with better inline fns

--- src/strukts_hashing.c
+++ src/strukts_hashing.c
@@ -3,77 +3,39 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strukts_types.h>
 
-/* rotates value to left by amount times (also known as circular shift) */
-static uint32_t strukts_rotate_left(uint32_t value, uint8_t amount) {
-    /* undef behavior for 0 or 32 */
-    if ((amount < 1) | (amount > 31)) exit(EXIT_FAILURE);
-
+/********************** STATIC INLINE FUNCTIONS **********************/
+static inline WORD rotate_left(WORD value, BYTE amount) {
     return value << amount | value >> (32 - amount);
 }
 
-static uint32_t strukts_build_remaining_chunk(const uint8_t* key, uint8_t remaining_bytes) {
-    uint32_t final_chunk = 0;
+static inline WORD mur(WORD hash, WORD block, bool final_rotate) {
+    const WORD c1 = 0xcc9e2d51;
+    const WORD c2 = 0x1b873593;
+    const BYTE r1 = 15;
+    const BYTE r2 = 13;
+    const BYTE m = 5;
+    const WORD n = 0xe6546b64;
 
-    switch (remaining_bytes) {
-        case 3:
-            memcpy(&final_chunk, key, 3 * sizeof(uint8_t));  // final 3 bytes
-            break;
+    block *= c1;
+    block = rotate_left(block, r1);
+    block *= c2;
+    hash ^= block;
 
-        case 2:
-            memcpy(&final_chunk, key, 2 * sizeof(uint8_t));  // final 2 bytes
-            break;
+    if (!final_rotate) return hash;
 
-        default:
-            memcpy(&final_chunk, key, sizeof(uint8_t));  // final byte
-            break;
-    }
+    hash = rotate_left(hash, r2);
+    hash = (hash * m) + n;
 
-    return final_chunk;
+    return hash;
 }
 
-uint32_t strukts_murmur3_hash(const uint8_t* key, size_t key_len, uint32_t seed) {
-    /* constants of the algorithm */
-    const uint32_t c1 = 0xcc9e2d51;
-    const uint32_t c2 = 0x1b873593;
-    const uint8_t r1 = 15;
-    const uint8_t r2 = 13;
-    const uint8_t m = 5;
-    const uint32_t n = 0xe6546b64;
-
-    /* algorithm local variables */
-    uint32_t key_chunk = 0; /* buffer of 4 bytes */
-    uint32_t hash = seed;
-
-    /* 4 bytes chunk processing */
-    for (size_t i = 0; i < key_len / 4; ++i) {
-        /* copies 4 bytes into key chunk */
-        memcpy(&key_chunk, key, sizeof(uint32_t));
-
-        /* changes key pointer base address by 4 bytes (+4 indexes positions) */
-        key += sizeof(uint32_t);
-
-        /* MUR */
-        key_chunk *= c1;
-        key_chunk = strukts_rotate_left(key_chunk, r1); /* ROL */
-        key_chunk *= c2;
-
-        hash ^= key_chunk;
-        hash = strukts_rotate_left(hash, r2);
-        hash = (hash * m) + n;
-    }
-
-    /* remaining bytes (1, 2 or 3 chunks of 4-bytes words) */
-    key_chunk = strukts_build_remaining_chunk(key, key_len & 3);
-
-    /* MUR */
-    key_chunk *= c1;
-    key_chunk = strukts_rotate_left(key_chunk, r1); /* ROL */
-    key_chunk *= c2;
-    hash ^= key_chunk;
-
-    /* final avalanche to guarantee that if a single bit is changed,
-     * then at least 50% of the hash value is changed. */
+static inline final_avalanche(WORD hash, size_t key_len) {
+    /*
+     * Final avalanche to guarantee that if a single bit of the key is changed,
+     * then at least 50% of the final hash value is also changed.
+     */
     hash ^= key_len;
     hash ^= (hash >> 16);
     hash *= 0x85ebca6b;
@@ -82,4 +44,37 @@ uint32_t strukts_murmur3_hash(const uint8_t* key, size_t key_len, uint32_t seed)
     hash ^= hash >> 16;
 
     return hash;
+}
+
+static inline WORD build_final_block(const BYTE* key, BYTE remaining_bytes) {
+    WORD final_block = 0;
+
+    /*
+     * Builds the final 32-bit block with possible remaining key bits. Notice that
+     * if key_len is a multiple of 4, then the final block is composed only of 0 bits.
+     */
+    if (remaining_bytes == 3) memcpy(&final_block, key, 3 * sizeof(BYTE));
+    if (remaining_bytes == 2) memcpy(&final_block, key, 2 * sizeof(BYTE));
+    if (remaining_bytes == 1) memcpy(&final_block, key, 1 * sizeof(BYTE));
+
+    return final_block;
+}
+
+/********************** PUBLIC FUNCTIONS **********************/
+WORD strukts_murmur3_hash(const BYTE* key, size_t key_len, WORD seed) {
+    WORD hash = seed;
+    WORD block = 0;
+
+    /* iterative block hashing of the whole key in 4-bytes blocks */
+    for (size_t i = 0; i < key_len / 4; ++i) {
+        memcpy(&block, key, sizeof(WORD));
+        hash = mur(hash, block, true);
+
+        key += sizeof(WORD);
+    }
+
+    block = build_final_block(key, key_len & 3);
+    hash = mur(hash, block, false);
+
+    return final_avalanche(hash, key_len);
 }
--- tests/test_strukts_hashing.cpp
+++ tests/test_strukts_hashing.cpp
@@ -4,17 +4,18 @@
 
 #include "gtest/gtest.h"
 #include "strukts_hashing.h"
+#include "strukts_types.h"
 
 namespace {
     TEST(STRUKTS_HASHING_SUITE, SHOULD_32BIT_MURMUR3_HASH_STRING_WITH_5_BYTES) {
         // arrange
         const char str[10] = "abcde";  // literal str: {'a', 'b', 'c', 'd', 'e', '\0'};
-        const uint8_t* str_bytes = (uint8_t*)str;
-        const uint32_t seed = 0;
+        const BYTE* str_bytes = (BYTE*)str;
+        const WORD seed = 0;
         size_t str_len = strlen(str);
 
         // act
-        uint32_t hash = strukts_murmur3_hash(str_bytes, str_len, seed);
+        WORD hash = strukts_murmur3_hash(str_bytes, str_len, seed);
 
         // assert
         EXPECT_EQ(hash, -392455434);  // expectation from python's lib mmh3
@@ -23,12 +24,12 @@ namespace {
     TEST(STRUKTS_HASHING_SUITE, SHOULD_32BIT_MURMUR3_HASH_STRING_WITH_2_BYTES) {
         // arrange
         const char str[10] = "ab";
-        const uint8_t* str_bytes = (uint8_t*)str;
-        const uint32_t seed = 5;
+        const BYTE* str_bytes = (BYTE*)str;
+        const WORD seed = 5;
         size_t str_len = strlen(str);
 
         // act
-        uint32_t hash = strukts_murmur3_hash(str_bytes, str_len, seed);
+        WORD hash = strukts_murmur3_hash(str_bytes, str_len, seed);
 
         // assert
         EXPECT_EQ(hash, 810406479);  // expectation from python's lib mmh3
@@ -37,12 +38,12 @@ namespace {
     TEST(STRUKTS_HASHING_SUITE, SHOULD_32BIT_MURMUR3_HASH_STRING_WITH_22_BYTES) {
         // arrange
         const char str[50] = "some really nice long key";
-        const uint8_t* str_bytes = (uint8_t*)str;
-        const uint32_t seed = 10;
+        const BYTE* str_bytes = (BYTE*)str;
+        const WORD seed = 10;
         size_t str_len = strlen(str);
 
         // act
-        uint32_t hash = strukts_murmur3_hash(str_bytes, str_len, seed);
+        WORD hash = strukts_murmur3_hash(str_bytes, str_len, seed);
 
         // assert
         EXPECT_EQ(hash, -464589223);  // expectation from python's lib mmh3
