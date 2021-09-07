aa28bfd073da06dc7e6825b545c2e3b5514b848a
IgooorGP <igor.feq@gmail.com>
2021-09-07 11:07:08 -0300

    feat(heapsort): adds heapsort algorithm

--- include/strukts/strukts_sorting.h
+++ include/strukts/strukts_sorting.h
@@ -15,7 +15,7 @@ extern "C" {
 #include <stdlib.h>
 
 /**
- * Performs an in-place sorting of the int array a using insertion sort. As insertion
+ * Performs an in-place sorting of an int array a using insertion sort. As insertion
  * sort runs in O(n^2), the len param is an int and not a size_t to avoid some complications
  * due to size_t underflows.
  *
@@ -27,7 +27,7 @@ extern "C" {
 bool strukts_sorting_insertionsort(int a[], int len);
 
 /**
- * Performs an in-place sorting of the int array using mergesort (algorithm based on the divide and
+ * Performs an in-place sorting of an int array using mergesort (algorithm based on the divide and
  * conquer strategy).
  *
  * @param a is an array of integers which will be sorted in place.
@@ -36,6 +36,16 @@ bool strukts_sorting_insertionsort(int a[], int len);
  * @return true if the sorting was successful; false, otherwise.
  */
 bool strukts_sorting_mergesort(int a[], size_t len);
+
+/**
+ * Performs an in-place sorting of an int array using heapsort.
+ *
+ * @param a is an array of integers which will be sorted in place.
+ * @param len is the length of array.
+ *
+ * @return true if the sorting was successful; false, otherwise.
+ */
+bool strukts_sorting_heapsort(int a[], size_t len);
 
 #ifdef __cplusplus
 }
