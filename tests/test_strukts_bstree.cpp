b7f31b624e1ebe0d2fd4b0aee5052e1c38527a14
IgooorGP <igor.feq@gmail.com>
2021-09-11 23:23:23 -0300

    test(bstree): add tests for searching

--- tests/test_strukts_bstree.cpp
+++ tests/test_strukts_bstree.cpp
@@ -1,5 +1,9 @@
+#include <string.h>
+
 #include "gtest/gtest.h"
 #include "strukts_bstree.h"
+
+#define str(s) ((char*)s)
 
 namespace
 {
@@ -12,6 +16,8 @@ namespace
         EXPECT_EQ(tree->height, 0);
         EXPECT_EQ(tree->size, 0);
         EXPECT_EQ(tree->root, nullptr);
+
+        strukts_bstree_free(tree);
     }
 
     TEST(STRUKTS_BSTREE_SUITE, SHOULD_CREATE_INSERT_ITEMS_ON_BINARY_SEARCH_TREE)
@@ -20,10 +26,10 @@ namespace
         StruktsBSTree* tree = strukts_bstree_new();
 
         /* act */
-        strukts_bstree_insert(tree, 12, (char*)"12");
-        strukts_bstree_insert(tree, 5, (char*)"5");
-        strukts_bstree_insert(tree, 18, (char*)"18");
-        strukts_bstree_insert(tree, 15, (char*)"15");
+        strukts_bstree_insert(tree, 12, str("12"));
+        strukts_bstree_insert(tree, 5, str("5"));
+        strukts_bstree_insert(tree, 18, str("18"));
+        strukts_bstree_insert(tree, 15, str("15"));
 
         /* assert */;
         EXPECT_EQ(tree->size, 4);
@@ -32,5 +38,32 @@ namespace
         EXPECT_EQ(tree->root->left->key, 5);
         EXPECT_EQ(tree->root->right->key, 18);
         EXPECT_EQ(tree->root->right->left->key, 15);
+
+        strukts_bstree_free(tree);
+    }
+
+    TEST(STRUKTS_BSTREE_SUITE, SHOULD_GET_FIND_KEY_BINARY_SEARCH_TREE)
+    {
+        /* arrange */
+        StruktsBSTree* tree = strukts_bstree_new();
+
+        strukts_bstree_insert(tree, 12, str("12"));
+        strukts_bstree_insert(tree, 5, str("5"));
+        strukts_bstree_insert(tree, 18, str("18"));
+        strukts_bstree_insert(tree, 15, str("15"));
+
+        /* act */
+        StruktsBSTNode* node = strukts_bstree_get(tree, 18);
+
+        /* assert - current node */
+        EXPECT_TRUE(node != nullptr);
+        EXPECT_EQ(node->key, 18);
+        EXPECT_TRUE(strcmp(node->value, str("18")) == 0);
+
+        /* assert - adjacent node's children */
+        EXPECT_EQ(node->left->key, 15);
+        EXPECT_TRUE(node->right == nullptr);
+
+        strukts_bstree_free(tree);
     }
 }  // namespace
