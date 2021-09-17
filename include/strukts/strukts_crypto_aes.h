effdd7304e7692fa63b3c2cf94473767abfc530c
IgooorGP <igor.feq@gmail.com>
2021-09-16 22:06:52 -0300

    feat(aes): add naive aes schedule (wip)

---
+++ include/strukts/strukts_crypto_aes.h
@@ -0,0 +1,21 @@
+/**
+ * @file strukts_crypto_aes.h
+ *
+ * @brief Module that contains the AES symmetric cipher.
+ */
+
+#ifndef STRUKTS_CRYPTO_AES_H
+#define STRUKTS_CRYPTO_AES_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdlib.h>
+
+#include "strukts_types.h"
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* STRUKTS_CRYPTO_AES_H */
---
+++ src/strukts_crypto_aes.c
@@ -0,0 +1,136 @@
+#include "strukts_crypto_aes.h"
+
+#include <stdbool.h>
+
+/********************** CONSTANTS **********************/
+/*
+ * AES S-box is used to perform byte substitutions to create diffusion. The AES S-box converts
+ * bytes (0 to 255 ints) to another value. As such, instead of a 16x16 2D array, a 256 1D array
+ * is used as it's fast substitute a byte by using its very own value as the index:
+ * sbox[byte_value].
+ *
+ * Example:
+ * >>> byte_val = 255; // max value for (8-bit) byte
+ * >>> subs_byte = sbox[byte_val]; // subs_byte <- 0x16
+ */
+static const BYTE sbox[256] = {
+    /*00   01    02    03    04    05    06    07    08    09    0a    0b    0c    0d    0e   0f*/
+    0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76,
+    0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0, 0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0,
+    0xb7, 0xfd, 0x93, 0x26, 0x36, 0x3f, 0xf7, 0xcc, 0x34, 0xa5, 0xe5, 0xf1, 0x71, 0xd8, 0x31, 0x15,
+    0x04, 0xc7, 0x23, 0xc3, 0x18, 0x96, 0x05, 0x9a, 0x07, 0x12, 0x80, 0xe2, 0xeb, 0x27, 0xb2, 0x75,
+    0x09, 0x83, 0x2c, 0x1a, 0x1b, 0x6e, 0x5a, 0xa0, 0x52, 0x3b, 0xd6, 0xb3, 0x29, 0xe3, 0x2f, 0x84,
+    0x53, 0xd1, 0x00, 0xed, 0x20, 0xfc, 0xb1, 0x5b, 0x6a, 0xcb, 0xbe, 0x39, 0x4a, 0x4c, 0x58, 0xcf,
+    0xd0, 0xef, 0xaa, 0xfb, 0x43, 0x4d, 0x33, 0x85, 0x45, 0xf9, 0x02, 0x7f, 0x50, 0x3c, 0x9f, 0xa8,
+    0x51, 0xa3, 0x40, 0x8f, 0x92, 0x9d, 0x38, 0xf5, 0xbc, 0xb6, 0xda, 0x21, 0x10, 0xff, 0xf3, 0xd2,
+    0xcd, 0x0c, 0x13, 0xec, 0x5f, 0x97, 0x44, 0x17, 0xc4, 0xa7, 0x7e, 0x3d, 0x64, 0x5d, 0x19, 0x73,
+    0x60, 0x81, 0x4f, 0xdc, 0x22, 0x2a, 0x90, 0x88, 0x46, 0xee, 0xb8, 0x14, 0xde, 0x5e, 0x0b, 0xdb,
+    0xe0, 0x32, 0x3a, 0x0a, 0x49, 0x06, 0x24, 0x5c, 0xc2, 0xd3, 0xac, 0x62, 0x91, 0x95, 0xe4, 0x79,
+    0xe7, 0xc8, 0x37, 0x6d, 0x8d, 0xd5, 0x4e, 0xa9, 0x6c, 0x56, 0xf4, 0xea, 0x65, 0x7a, 0xae, 0x08,
+    0xba, 0x78, 0x25, 0x2e, 0x1c, 0xa6, 0xb4, 0xc6, 0xe8, 0xdd, 0x74, 0x1f, 0x4b, 0xbd, 0x8b, 0x8a,
+    0x70, 0x3e, 0xb5, 0x66, 0x48, 0x03, 0xf6, 0x0e, 0x61, 0x35, 0x57, 0xb9, 0x86, 0xc1, 0x1d, 0x9e,
+    0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf,
+    0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16,
+};
+
+/* i = 1 .. 10 are valid (i = 0 is a filler) */
+static const BYTE round_constants[11] = {0x00, 0x01, 0x02, 0x04, 0x08, 0x10,
+                                         0x20, 0x40, 0x80, 0x1b, 0x36};
+
+typedef enum { Aes128, Aes192, Aes256 } AesType;
+
+typedef struct {
+    AesType type;
+    WORD key[60];
+    int size; /* AES-128: 44 words, AES-192: 52 words, AES-256: 60 words */
+} StruktsAesExpandedKey;
+
+/********************** PRIVATE FUNCTIONS **********************/
+static inline void rotate_split_word(BYTE split_word[])
+{
+    split_word[0] = split_word[1];
+    split_word[1] = split_word[2];
+    split_word[2] = split_word[3];
+    split_word[3] = split_word[0];
+}
+
+static inline void move(WORD word, BYTE* split_word)
+{
+    short int j = 0;
+
+    for (short int i = 0, j = 3; i < 4; i++, j--)
+        split_word[i] = word >> 8 * j; /* shifts 24, 16, 8, 0 */
+}
+
+static inline void unmove(BYTE* split_word, WORD* word)
+{
+    for (size_t i = 0; i < 4; i++) {
+        word* = split_word[i];
+        word* << 8;
+    }
+}
+
+static inline void sbox_split_word(BYTE split_word[])
+{
+    for (short int i = 0; i < 4; i++)
+        split_word[i] = sbox[split_word[i]];
+}
+
+static inline WORD round_constant(int i)
+{
+    WORD word = round_constants[i];
+    return word << 24;
+}
+
+static inline StruktsAesExpandedKey expanded_key_new(size_t original_key_size)
+{
+    AesType type;
+    int expanded_size;
+
+    switch (original_key_size) {
+        case 8: /* 8 words of 32 bits each = 256 bits */
+            type = Aes256;
+            expanded_size = 60;
+            break;
+        case 6: /* 6 words = 192 bits */
+            type = Aes192;
+            expanded_size = 52;
+            break;
+        default: /* 4 words = 128 bits */
+            type = Aes128;
+            expanded_size = 44;
+            break;
+    }
+
+    return (StruktsAesExpandedKey){.key = {0}, .size = expanded_size, .type = type};
+}
+
+StruktsAesExpandedKey strukts_crypto_aes_expand_key(WORD* private_key, size_t key_size)
+{
+    StruktsAesExpandedKey expanded_key = expanded_key_new(key_size);
+    BYTE split_word[4] = {0};
+    WORD word = 0;
+
+    for (size_t i = 0; i < expanded_key.size; i++) {
+        if (i < key_size) {
+            expanded_key.key[i] = private_key[i];
+        } else if (i % expanded_key.size == 0) {
+            move(private_key[i - 1], split_word);
+            rotate_split_word(split_word);
+            sbox_split_word(split_word);
+            unmove(split_word, &word);
+            expanded_key.key[i] =
+                expanded_key[i - key_size] ^ word ^ round_constant(i / expanded_key.size);
+        } else if (key_size > 6 && i % key_size == 4) {
+            move(private_key[i - 1], split_word);
+            sbox_split_word(split_word);
+            unmove(split_word, word);
+            expanded_key.key[i] = expanded_key[i - key_size] ^ word;
+        } else {
+            expanded_key.key[i] = expanded_key[i - key_size] ^ expanded_key[i - 1];
+        }
+    }
+
+    /* expanded_key is to be used every 4 words to make up a key of 128 bits (4 * 32) every round */
+    return expanded_key;
+}
