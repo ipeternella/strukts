c3d8d887d6c8065c5f91fa46249e927356015a23
IgooorGP <igor.feq@gmail.com>
2021-09-22 23:02:42 -0300

    chore(rbtree): add insert with color fix function

--- include/strukts/strukts_rbtree.h
+++ include/strukts/strukts_rbtree.h
@@ -52,7 +52,35 @@ struct _StruktsRBTree {
  *
  * @return a pointer to an empty red black tree.
  */
-StruktsRBTNode* strukts_rbtree_new();
+StruktsRBTree* strukts_rbtree_new();
+
+/**
+ * Inserts a new node (red) on a red black tree. This function fixes up the
+ * red black tree properties after the insertion.
+ *
+ * @param key is the key of the new node.
+ * @param value is the value associated with the new node.
+ *
+ * @return true if the insertion was successful; otherwise, returns false in case
+ * an allocation error happened.
+ */
+bool strukts_rbtree_insert(StruktsRBTree* tree, int key, char* value);
+
+/**
+ * Computes the height of the given red black tree. The notion of 'height' used here
+ * is the based on edges count (connection between two nodes) and not on nodes count. Possible
+ * scenarios:
+ *
+ * - if the tree contains a nil_node root, the height is -1;
+ * - if the tree contains a single node (just the root), the height is 0;
+ * - if the tree contains more than one node (root + others), the height is > 0.
+ *
+ * @param tree is the tree which contains a reference to the nil_node of the r.b.tree.
+ * @param root_node is the root of the tree to start computing the height.
+ *
+ * @return the size of the tree based on edge counting (not node counting).
+ */
+int strukts_rbtree_height(StruktsRBTree* tree, StruktsRBTNode* root);
 
 #ifdef __cplusplus
 }
--- src/strukts_rbtree.c
+++ src/strukts_rbtree.c
@@ -3,6 +3,14 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+/********************** MACROS **********************/
+#define max(a, b)               \
+    ({                          \
+        __typeof__(a) _a = (a); \
+        __typeof__(b) _b = (b); \
+        _a > _b ? _a : _b;      \
+    })
+
 /********************** PRIVATE FUNCTIONS **********************/
 static inline StruktsRBTNode* rbtree_nil_node_new()
 {
@@ -11,7 +19,7 @@ static inline StruktsRBTNode* rbtree_nil_node_new()
     if (nil_node == NULL)
         return NULL;
 
-    nil_node->color = Black;
+    nil_node->color = Black; /* nil nodes are black */
     nil_node->key = 0;
     nil_node->parent = NULL;
     nil_node->left = NULL;
@@ -19,6 +27,23 @@ static inline StruktsRBTNode* rbtree_nil_node_new()
     nil_node->value = NULL;
 
     return nil_node;
+}
+
+static inline StruktsRBTNode* rbtree_node_new(StruktsRBTree* tree, int key, char* value)
+{
+    StruktsRBTNode* new_node = (StruktsRBTNode*)malloc(sizeof(StruktsRBTNode));
+
+    if (new_node == NULL)
+        return NULL;
+
+    new_node->color = Red; /* new nodes are red to have two black children and keep black height */
+    new_node->key = key;
+    new_node->value = value;
+    new_node->parent = tree->nil_node;
+    new_node->left = tree->nil_node;
+    new_node->right = tree->nil_node;
+
+    return new_node;
 }
 
 static inline bool is_left_child(StruktsRBTNode* node)
@@ -51,7 +76,7 @@ static void rbtree_left_rotate(StruktsRBTree* tree, StruktsRBTNode* node)
 
     if (node->parent == tree->nil_node)
         tree->root = pivot_node;
-    else if (node == node->parent.left)
+    else if (node == node->parent->left)
         node->parent->left = pivot_node;
     else
         node->parent->right = pivot_node;
@@ -90,7 +115,7 @@ static void rbtree_right_rotate(StruktsRBTree* tree, StruktsRBTNode* node)
     node->parent = pivot_node;
 }
 
-static void strukts_rbtree_insert_fix(StruktsRBTree* tree, StruktsRBTNode* node)
+static void rbtree_insert_fix(StruktsRBTree* tree, StruktsRBTNode* node)
 {
     StruktsRBTNode* right_uncle;
     StruktsRBTNode* left_uncle;
@@ -162,7 +187,7 @@ static void strukts_rbtree_insert_fix(StruktsRBTree* tree, StruktsRBTNode* node)
 }
 
 /********************** PUBLIC FUNCTIONS **********************/
-StruktsRBTNode* strukts_rbtree_new()
+StruktsRBTree* strukts_rbtree_new()
 {
     StruktsRBTree* tree = (StruktsRBTree*)malloc(sizeof(StruktsRBTree));
 
@@ -180,4 +205,51 @@ StruktsRBTNode* strukts_rbtree_new()
     tree->nil_node = nil_node;
 
     return tree;
+}
+
+int strukts_rbtree_height(StruktsRBTree* tree, StruktsRBTNode* root)
+{
+    if (root == NULL || root == NULL || root == tree->nil_node)
+        return -1;
+
+    return max(strukts_rbtree_height(tree, root->left), strukts_rbtree_height(tree, root->right)) +
+           1;
+}
+
+bool strukts_rbtree_insert(StruktsRBTree* tree, int key, char* value)
+{
+    /* creates a red node with tree->nil_node left and right children */
+    StruktsRBTNode* new_node = rbtree_node_new(tree, key, value);
+
+    if (new_node == NULL)
+        return false; /* allocation error */
+
+    StruktsRBTNode* next = tree->root; /* start looking at the root of the tree */
+    StruktsRBTNode* parent = tree->nil_node;
+
+    /* parent will hold the new node's parent */
+    while (next != tree->nil_node) {
+        parent = next;
+
+        if (key < next->key)
+            next = next->left;
+        else
+            next = next->right;
+    }
+
+    /* sets new node's parent */
+    new_node->parent = parent;
+
+    /* sets parent reference */
+    if (parent == tree->nil_node)
+        tree->root = new_node;
+    else if (key < parent->key)
+        parent->left = new_node;
+    else
+        parent->right = new_node;
+
+    /* as we might have a red child whose parent is also red, we need to fix the r.b.tree */
+    rbtree_insert_fix(tree, new_node);
+
+    return true;
 }
